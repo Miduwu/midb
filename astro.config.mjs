104498da3a1f6f9cc30c97ab5f5811e6851e27e0
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2025-02-05 20:16:56 -0600

    mods

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://miduwu.github.io/midb',
+    integrations: [
+        starlight({
+            title: 'midb - a modern way',
+            favicon: './public/icon_white.png',
+            logo: {
+                src: './public/icon_white.png'
+            },
+            social: {
+				discord: 'https://discord.com/invite/dKhuDRW8dB',
+				github: 'https://github.com/Miduwu/midb',
+			},
+        })
+    ]
+});
